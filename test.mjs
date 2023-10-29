c99ac8dd60c7a3ea1687eeaa2ef6579ba4132db4
sujin <sujin3473@gmail.com>
2023-10-29 19:11:51 +0900

    linked-list

---
+++ test.mjs
@@ -0,0 +1,13 @@
+import { Node, LinkedList } from "./linked-list/LinkedList.mjs";
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+node1.next = node2;
+node2.next = node3;
+
+console.log(node1.data);
+console.log(node1.next.data);
+console.log(node1.next.next.data);
+
